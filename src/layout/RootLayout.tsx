4e298a0f1106d8156d77728367bd1fea781de402
mehedihasan101101 <sojibkhan39621@gmail.com>
2026-03-19 06:07:13 +0000

    create useThemeController hook

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -3,20 +3,23 @@ import Nav from "../components/nav/Nav";
 import { IoMdSettings } from "react-icons/io";
 import { motion } from "motion/react"
 import { useState } from "react";
+import useThemeControll from "../utilities/hooks/useThemeControll";
 
 
-
-const VariantThemeControllerTab = {
+// variant for ThemeController animation
+const ThemeControllerVariant = {
     hidden: {
         x: "100%",
         transition: {
             type: "tween" as const,
+            duration: .2
         }
     },
     visible: {
         x: "0",
         transition: {
             type: "tween" as const,
+            duration: .2
 
         }
     }
@@ -24,14 +27,13 @@ const VariantThemeControllerTab = {
 
 const RootLayout = () => {
 
-    const [isThemeControllerOpen, setIsThemeControllerOpen] = useState(false)
-
-    function handleThemeControllerState() {
-        setIsThemeControllerOpen(!isThemeControllerOpen)
+    const [isThemeTabOpen, setThemeTabOpen] = useState(false)
+    // handle open & close state of theme controller
+    function handleThemeTab() {
+        setThemeTabOpen(!isThemeTabOpen)
     }
 
-
-
+    const { setLightMode, setDarkMode } = useThemeControll()
     return (
         <div className="flex relative overflow-x-hidden">
             <Nav></Nav>
@@ -40,11 +42,20 @@ const RootLayout = () => {
             </main>
 
             {/* theme control tab */}
-            <motion.div variants={VariantThemeControllerTab} initial="hidden" animate={isThemeControllerOpen ? "visible" : "hidden"} className="bg-themeSettingTabBg absolute right-0 top-1/4 rounded-bl-md will-change-transform">
+            <motion.div variants={ThemeControllerVariant} initial="hidden" animate={isThemeTabOpen ? "visible" : "hidden"} className="bg-themeSettingTabBg absolute right-0 top-1/4 rounded-bl-md will-change-transform">
                 <div className="absolute bg-themeSettingTabBg left-0 -translate-x-[90%]  h-13 w-13 flex items-center justify-center rounded-bl-md rounded-l-md">
-                    <button onClick={handleThemeControllerState}><IoMdSettings className=" text-sideBarBgColor text-4xl "></IoMdSettings></button>
+                    <button onClick={handleThemeTab}><IoMdSettings className=" text-sideBarBgColor text-4xl "></IoMdSettings></button>
                 </div>
-                <div className="w-30  h-50 relative">
+                <div className="w-45 px-4  h-50 relative text-textThemeController font-bold">
+                    <div>
+                        <h3 className=" mt-13 "> Theme Color</h3>
+
+                        <div className="flex gap-2">
+                            <button onClick={setLightMode} className="w-9 h-9 bg-white rounded-full cursor-pointer"></button>
+                            <button onClick={setDarkMode} className="w-9 h-9 bg-red-300 rounded-full cursor-pointer"></button>
+                        </div>
+
+                    </div>
 
                 </div>
             </motion.div>
